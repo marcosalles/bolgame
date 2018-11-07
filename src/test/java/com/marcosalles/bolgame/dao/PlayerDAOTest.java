da83c2cd674822d6508d543d36610b7d1bd6fa6d
Marco Salles <marcosalles@gmail.com>
2018-11-06 21:16:41 -0300

    testing player save without id

--- src/test/java/com/marcosalles/bolgame/dao/PlayerDAOTest.java
+++ src/test/java/com/marcosalles/bolgame/dao/PlayerDAOTest.java
@@ -1,13 +1,26 @@
 package com.marcosalles.bolgame.dao;
 
-import org.junit.jupiter.api.Test;
+import com.marcosalles.bolgame.model.entity.Player;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class PlayerDAOTest {
 
-class PlayerDAOTest {
+	@Autowired
+	private PlayerDAO playerDAO;
 
-	@Test
-	void component__should_not_need_testing() {
-		assertThat("This component does not have any extra implementations so the test suite from springframework should suffice.", true);
+	@Test(expected = JpaSystemException.class)
+	public void save__should_not_save_without_id() {
+		final var player = Player.builder().username("player").build();
+		playerDAO.save(player);
 	}
 }
